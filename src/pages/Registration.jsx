774e9e93f1e4871d27a6a0dc56b384eecbb0291b
washington <washingtonaraujocarvalho@gmail.com>
2025-11-10 22:21:47 -0300

    adicionado a page de cadastro junto com sua estilização

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -4,9 +4,33 @@ function Registration() {
   return (
     <>
       <body>
-        <div className="background-blobs">
-          <div className="blob blob-1"></div>
-          <div className="blob blob-2"></div>
+        <div>
+          <form action="" className="card-cadastro">
+            <div className="bolinha">
+              <h3>Cadastro de Usuário</h3>
+            </div>
+            <div>
+              <label for="username">Username:</label>
+              <input type="text" id="username" value="Bryan Brasileiro"></input>
+            </div>
+            <div>
+              <label for="password">Password:</label>
+              <input type="password" id="password" value="***********"></input>
+            </div>
+            <div>
+              <label for="birthdate">Data de nascimento:</label>
+              <input type="text" id="birthdate" placeholder="DD/MM/YY"></input>
+            </div>
+            <div>
+              <label for="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value="bryanarrombado666@gmail.com"
+              />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
       </body>
     </>
